f46a0c2c6fbd0d24bceda2489672196a256af5ad
JaeranKIM <devjaeran@gmail.com>
2022-12-19 15:50:11 +0900

    FEAT: 시간 제공 기능 구현

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,11 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-  console.log("Hello!");
+function getClock() {
+  const date = new Date();
+  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-setTimeout(sayHello, 5000);
+//getClock 호출
+// -> 호출하지 않고 setInterval을 실행하면 맨 처음 화면에 00:00:00이 뜬다.
+getClock();
+setInterval(getClock, 1000); //매 초마다 getClock 다시실행
